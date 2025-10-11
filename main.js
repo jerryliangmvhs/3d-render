2c5ab7db0180c06f2e3de2c3fae8b96491a66205
small-j1337 <jerryliang.mv@gmail.com>
2025-10-11 12:28:05 -0700

    Enable Anti-aliasing

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -19,7 +20,7 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 */
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry(2,1,2);
 
 const materials = [
   new THREE.MeshBasicMaterial({ color: 0xff0000 }), // front
@@ -40,9 +41,7 @@ camera.rotation.x = -0.5;
 
 function animate(){
     requestAnimationFrame(animate);
-    cube.rotation.y += 0.01;
     renderer.render(scene,camera);
-
 }
 
 window.addEventListener('resize', () => {
