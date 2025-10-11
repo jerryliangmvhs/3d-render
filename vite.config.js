627e45ee4b5af0aafa525ae477a60f72792909f4
small-j1337 <jerryliang.mv@gmail.com>
2025-10-11 11:33:37 -0700

    Update index.html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
-/** @type {import ('vite').UserConfig} */
 
-export default {
-    base: process.env.NODE_ENV === 'production' ? '/3d-render/' : '/'
-}
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/3d-render/' : '/',
+});
