c5fb47004ac0659b7a214a0e07ced785863d869a
small-j1337 <jerryliang.mv@gmail.com>
2025-10-12 13:51:02 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'; // fixed import path and duplicate import
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 const scene = new THREE.Scene();
